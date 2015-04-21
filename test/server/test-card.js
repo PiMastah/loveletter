a86aaf73f7d23bf10f1bbccd25a9e0bf9790d433
Chimney42 <draggili@gmail.com>
2015-04-21 21:34:11 +0200

    use correct test method

--- test/server/test-card.js
+++ test/server/test-card.js
@@ -1,5 +1,5 @@
 var buster = require("buster");
-var expect = buster.expect;
+var assert = buster.assert;
 
 var cardFactory = require('../../src/server/card');
 
@@ -10,6 +10,6 @@ describe("A card", function () {
         var rank = 1;
         var card = cardFactory.create(rank);
 
-        expect(card.rank).toBe(rank);
+        assert.same(card.rank, rank);
     });
 });
