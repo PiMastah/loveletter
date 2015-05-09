fd875ae8e129f371878240687f4f6bd84a2b0466
PiMastah <pimastah@gmail.com>
2015-05-09 02:53:10 +0200

    [STYLE] fixed tabs indenting lines

--- src/server/policy/deal.js
+++ src/server/policy/deal.js
@@ -1,12 +1,12 @@
 module.exports = function (state, dealer) {
-	state.discardedCards = Array.apply(null, new Array(4)).map(function () {
-		return dealer.drawCard();
-	});
-	state.hands = state.players.map(function (player, index) {
-		return [dealer.drawCard()];
-	});
-	state.deck = Array.apply(null, new Array(dealer.deck.length)).map(function () {
-		return dealer.drawCard();
-	});
-	return state;
+    state.discardedCards = Array.apply(null, new Array(4)).map(function () {
+        return dealer.drawCard();
+    });
+    state.hands = state.players.map(function (player, index) {
+        return [dealer.drawCard()];
+    });
+    state.deck = Array.apply(null, new Array(dealer.deck.length)).map(function () {
+        return dealer.drawCard();
+    });
+    return state;
 };
