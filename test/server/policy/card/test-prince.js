0a0084eb3632221d0e8a00548cb7e0ce7a14be28
PiMastah <pimastah@gmail.com>
2015-05-09 20:44:58 +0200

    [FEATURE] adds placeholder for remaining card policies

---
+++ test/server/policy/card/test-prince.js
@@ -0,0 +1,18 @@
+var buster = require("buster");
+var assert = buster.assert;
+var refute = buster.refute;
+
+var cardPolicy = require('../../../../src/server/policy/card/prince');
+var stateFactory = require('../../../../src/server/gamestate');
+var cardFactory = require('../../../../src/server/card');
+
+buster.spec.expose();
+
+describe("A Prince", function () {
+    var self = this;
+    buster.spec.before(function () {
+    });
+
+    it("//acts", function () {
+    });
+});
